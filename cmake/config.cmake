68917ca42b9780b288b5039a07704e1391407b8c
Aaron Barany <akb825@gmail.com>
2016-03-12 19:38:22 -0800

    Added project and source filters.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -18,3 +18,43 @@ else()
 endif()
 
 enable_testing()
+
+function(set_folder target folderName)
+	if (DEEPSEA_ROOT_FOLDER AND folderName)
+		set_property(TARGET ${target} PROPERTY FOLDER ${DEEPSEA_ROOT_FOLDER}/${folderName})
+	elseif (NOT DEEPSEA_ROOT_FOLDER AND folderName)
+		set_property(TARGET ${target} PROPERTY FOLDER ${folderName})
+	else()
+		set_property(TARGET ${target} PROPERTY FOLDER ${DEEPSEA_ROOT_FOLDER})
+	endif()
+endfunction()
+
+function(setup_filters)
+	set(options)
+	set(oneValueArgs SRC_DIR INCLUDE_DIR)
+	set(multiValueArgs FILES)
+	cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+	
+	foreach (fileName ${ARGS_FILES})
+		# Get the directory name. Make sure there's a trailing /.
+		get_filename_component(directoryName ${fileName} DIRECTORY)
+		set(directoryName ${directoryName}/)
+		
+		set(filterName)
+		string(REGEX MATCH ${ARGS_SRC_DIR}/.* matchSrc ${directoryName})
+		string(REGEX MATCH ${ARGS_INCLUDE_DIR}/.* matchInclude ${directoryName})
+		
+		if (matchSrc)
+			string(REPLACE ${ARGS_SRC_DIR}/ "" filterName ${directoryName})
+			set(filterName src/${filterName})
+		elseif (matchInclude)
+			string(REPLACE ${ARGS_INCLUDE_DIR}/ "" filterName ${directoryName})
+			set(filterName include/${filterName})
+		endif()
+		
+		if (filterName)
+			string(REPLACE "/" "\\" filterName ${filterName})
+			source_group(${filterName} FILES ${fileName})
+		endif()
+	endforeach()
+endfunction()
