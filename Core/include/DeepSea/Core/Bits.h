78fcfdee2ef4644ba946207aab1a2543ef169326
Aaron Barany <akb825@gmail.com>
2016-12-11 16:55:48 -0800

    Added bitmask helper functions.

---
+++ Core/include/DeepSea/Core/Bits.h
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2016 Aaron Barany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <DeepSea/Core/Config.h>
+#include <DeepSea/Core/Types.h>
+
+#if DS_MSC
+#include <intrin.h>
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**
+ * @file
+ * @brief Helper functions for manipulating bitmasks.
+ */
+
+/**
+ * @brief Counts the leading zeros in a 32-bit integer.
+ * @param x The bitmask.
+ * @return The number of leading zeros.
+ */
+inline uint32_t dsClz(uint32_t x);
+
+/**
+ * @brief Counts the trailing zeros in a 32-bit integer.
+ * @param x The bitmask.
+ * @return The number of trailing zeros.
+ */
+inline uint32_t dsCtz(uint32_t x);
+
+/**
+ * @brief Gets the index of the next item in a bitmask.
+ *
+ * This is used when iterating over a bitmask. For example:
+ * @code
+ * for (uint32_t curBitmask = bitmask; curBitmask; curBitmask = dsRemoveLastBit(curBitmask))
+ * {
+ *     uint32_t i = dsBitmaskIndex(curBitmask);
+ *     ...
+ * }
+ * @endcode
+ *
+ * @param x The bitmask.
+ * @return The first set bit.
+ */
+inline uint32_t dsBitmaskIndex(uint32_t x);
+
+/**
+ * @brief Removes the last bit in a bitmask.
+ *
+ * This is used when iterating over a bitmask. For example:
+ * @code
+ * for (uint32_t curBitmask = bitmask; curBitmask; curBitmask = dsRemoveLastBit(curBitmask))
+ * {
+ *     uint32_t i = dsBitmaskIndex(curBitmask);
+ *     ...
+ * }
+ * @endcode
+ *
+ * @param x The bitmask.
+ * @return The bitmask with the last bit removed.
+ */
+inline uint32_t dsRemoveLastBit(uint32_t x);
+
+inline uint32_t dsClz(uint32_t x)
+{
+#if DS_MSC
+	if (!x)
+		return 32;
+
+	DWORD ret = 0;
+	_BitScanReverse( &ret, x);
+	return ret;
+#elif DS_GCC || DS_CLANG
+	return x ? __builtin_clz(x) : 32;
+#else
+#error Need to implement clz for current compiler.
+#endif
+}
+
+inline uint32_t dsCtz(uint32_t x)
+{
+#if DS_MSC
+	if (!x)
+		return 32;
+
+	DWORD ret = 0;
+	_BitScanForward( &ret, x);
+	return ret;
+#elif DS_GCC || DS_CLANG
+	return x ? __builtin_ctz(x) : 32;
+#else
+#error Need to implement ctz for current compiler.
+#endif
+}
+
+inline uint32_t dsBitmaskIndex(uint32_t x)
+{
+	return dsCtz(x);
+}
+
+inline uint32_t dsRemoveLastBit(uint32_t x)
+{
+	return x & (x - 1);
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Core/test/BitsTest.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 Aaron Barany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <DeepSea/Core/Bits.h>
+#include <gtest/gtest.h>
+
+TEST(BitsTest, Clz)
+{
+	EXPECT_EQ(32U, dsClz(0));
+	EXPECT_EQ(31U, dsClz(1));
+	EXPECT_EQ(4U, dsClz(0x0FFFFFFF));
+	EXPECT_EQ(0U, dsClz(0xFFFFFFF0));
+}
+
+TEST(BitsTest, Ctz)
+{
+	EXPECT_EQ(32U, dsCtz(0));
+	EXPECT_EQ(0U, dsCtz(1));
+	EXPECT_EQ(0U, dsCtz(0x0FFFFFFF));
+	EXPECT_EQ(4U, dsCtz(0xFFFFFFF0));
+}
+
+TEST(BitsTest, BitmaskIterate)
+{
+	uint32_t bitmask = 0x39;
+	EXPECT_EQ(0U, dsBitmaskIndex(bitmask));
+
+	bitmask = dsRemoveLastBit(bitmask);
+	EXPECT_EQ(0x38, bitmask);
+	EXPECT_EQ(3U, dsBitmaskIndex(bitmask));
+
+	bitmask = dsRemoveLastBit(bitmask);
+	EXPECT_EQ(0x30, bitmask);
+	EXPECT_EQ(4U, dsBitmaskIndex(bitmask));
+
+	bitmask = dsRemoveLastBit(bitmask);
+	EXPECT_EQ(0x20, bitmask);
+	EXPECT_EQ(5U, dsBitmaskIndex(bitmask));
+
+	bitmask = dsRemoveLastBit(bitmask);
+	EXPECT_EQ(0U, bitmask);
+}
