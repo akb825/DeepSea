414e008eb2daba2c142586bfe90c44426ce30d4c
Aaron Barany <akb825@gmail.com>
2018-09-18 23:15:02 -0700

    Fix for finding FlatBuffers when cross-compiling.

--- cmake/ConvertFlatbuffers.cmake
+++ cmake/ConvertFlatbuffers.cmake
@@ -20,6 +20,10 @@ endif()
 # This will try to find the include directory on the system, and fall back to the version checked
 # into the repository.
 find_path(FLATBUFFERS_INCLUDE_DIRS flatbuffers/flatbuffers.h PATHS ${DEEPSEA_SOURCE_DIR}/external)
+if (NOT FLATBUFFERS_INCLUDE_DIRS)
+	set(FLATBUFFERS_INCLUDE_DIRS ${DEEPSEA_SOURCE_DIR}/external CACHE PATH
+		"FlatBuffers include directory" FORCE)
+endif()
 
 # ds_convert_flatbuffers(container
 #                        FILE file1 [file2 ...]
