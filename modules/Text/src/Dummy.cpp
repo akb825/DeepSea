c500a2295d29d5c7ca56131c99dbb1c2106c8511
Aaron Barany <akb825@gmail.com>
2019-02-16 13:38:46 -0800

    CMake improvements.
    Respect BUILD_SHARED_LIBS CMake variable and use LINKER_LANGUAGE property
    instead of a dummy .cpp file.

--- modules/Text/src/Dummy.cpp
+++
@@ -1,3 +0,0 @@
-// This is present to force C++ linkage.
-// When building as a shared library and statically linking HarfBuzz, this is needed to ensure that
-// it links with the C++ standard library.
