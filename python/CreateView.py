62c2b8e17208d4199dd4f3c6fa1970e7e4d30bdd
Aaron Barany <akb825@gmail.com>
2020-04-24 15:26:41 -0700

    Added top-level scene convert scripts.
    Added top-level scripts to convert scene resources, scenes, and views.

---
+++ python/CreateView.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Copyright 2020 Aaron Barany
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+import argparse
+import json
+from importlib import import_module
+
+from DeepSeaScene.Convert.ConvertContext import ConvertContext
+from DeepSeaScene.Convert.ViewConvert import convertView
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description =
+		'Create a vew to be used by Deep Sea.')
+	parser.add_argument('-i', '--input', required = True,
+		help = 'input json description of the resources')
+	parser.add_argument('-o', '--output', required = True,
+		help = 'output file name, typically with the extension ".dsv"')
+	parser.add_argument('-e', '--extension', nargs = '*', default = [],
+		help = 'list of module names for extensions. Eeach extension should have a '
+			'deepSeaSceneExtension(convertContext) function to register the custom types with the'
+			'convert context.')
+
+	args = parser.parse_args()
+	convertContext = ConvertContext(args.cuttlefish, args.vfc, args.multithread)
+	for extension in args.extensions:
+		import_module(extension).deepSeaSceneExtension(convertContext)
+
+	with open(args.input) as f:
+		data = json.load(f)
+
+	with open(args.output, 'wb') as f:
+		f.write(convertView(convertContext, data))
