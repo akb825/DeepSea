424b5570d78eeaaa4e7eaf28866e0a6d6e177d11
Aaron Barany <akb825@gmail.com>
2024-11-17 16:02:40 -0800

    Added support for Metal 3.2.
    Added missing CMake properties for Metal 3.0 and 3.1.

--- modules/Render/RenderMetal/msl-config/ConfigPaths.cmake
+++ modules/Render/RenderMetal/msl-config/ConfigPaths.cmake
@@ -6,6 +6,9 @@ set_property(GLOBAL PROPERTY metal-ios-2.1 ${CMAKE_CURRENT_LIST_DIR}/metal-ios-2
 set_property(GLOBAL PROPERTY metal-ios-2.2 ${CMAKE_CURRENT_LIST_DIR}/metal-ios-2.2.conf)
 set_property(GLOBAL PROPERTY metal-ios-2.3 ${CMAKE_CURRENT_LIST_DIR}/metal-ios-2.3.conf)
 set_property(GLOBAL PROPERTY metal-ios-2.4 ${CMAKE_CURRENT_LIST_DIR}/metal-ios-2.4.conf)
+set_property(GLOBAL PROPERTY metal-ios-3.0 ${CMAKE_CURRENT_LIST_DIR}/metal-ios-3.0.conf)
+set_property(GLOBAL PROPERTY metal-ios-3.1 ${CMAKE_CURRENT_LIST_DIR}/metal-ios-3.1.conf)
+set_property(GLOBAL PROPERTY metal-ios-3.2 ${CMAKE_CURRENT_LIST_DIR}/metal-ios-3.2.conf)
 
 set_property(GLOBAL PROPERTY metal-macos-1.1 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-1.1.conf)
 set_property(GLOBAL PROPERTY metal-macos-1.2 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-1.2.conf)
@@ -14,3 +17,6 @@ set_property(GLOBAL PROPERTY metal-macos-2.1 ${CMAKE_CURRENT_LIST_DIR}/metal-mac
 set_property(GLOBAL PROPERTY metal-macos-2.2 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-2.2.conf)
 set_property(GLOBAL PROPERTY metal-macos-2.3 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-2.3.conf)
 set_property(GLOBAL PROPERTY metal-macos-2.4 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-2.4.conf)
+set_property(GLOBAL PROPERTY metal-macos-3.0 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-3.0.conf)
+set_property(GLOBAL PROPERTY metal-macos-3.1 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-3.1.conf)
+set_property(GLOBAL PROPERTY metal-macos-3.2 ${CMAKE_CURRENT_LIST_DIR}/metal-macos-3.2.conf)
